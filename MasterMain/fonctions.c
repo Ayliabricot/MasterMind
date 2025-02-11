a7a9747fa04cee01c9f756befc67a4154c47927f
Aylia <aylia.sabadie@student.junia.com>
2025-02-11 13:35:58 +0100

    AH

--- MasterMain/fonctions.c
+++ MasterMain/fonctions.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "header.h"
+#include <time.h>
+
+Combinaison* creerCombinaison(int taille) {
+	Combinaison* nouvelle_combinaison = malloc(taille * sizeof(Combinaison));
+	nouvelle_combinaison = NULL;
+
+	return nouvelle_combinaison;
+}
+
+void definirCouleurs(Combinaison* combinaison) {
+	combinaison->couleur[0] = "rouge";
+	combinaison->couleur[1] = "vert";
+	combinaison->couleur[2] = "bleu";
+	combinaison->couleur[3] = "jaune";
+	combinaison->couleur[4] = "orange";
+	combinaison->couleur[5] = "violet";
+	combinaison->couleur[6] = "rose";
+	combinaison->couleur[7] = "blanc";
+}
+
+void assignerCouleur(Combinaison* combinaison, int index, char couleur[50]) {
+	combinaison->couleur[index] = couleur;
+}
 
--- MasterMain/main.c
+++ MasterMain/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "header.h"
+#include <time.h>
 
 int main() {
-
+	Combinaison* couleurs = creerCombinaison(10);
+	definirCouleurs(couleurs);
+	for (int i = 0; i < 10; i++) {
+		printf("%s / ", couleurs->couleur[i]);
+	}
 
 	return 0;
 }
